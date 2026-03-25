b56c419fa46d898446a13fc4bf136f5ff0de3ccf
Rogério B. Martins <rogeriodelphi@gmail.com>
2026-03-25 02:50:27 -0300

    Criando o modelo de Turismo

--- turismos/models.py
+++ turismos/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+
+class BaseModel(models.Model):
+    nome = models.CharField(max_length=200)
+    descricao = models.TextField()
+    endereco = models.CharField(max_length=255)
+    telefone = models.CharField(max_length=20, blank=True)
+
+    # 📍 LOCALIZAÇÃO (MAPA)
+    latitude = models.FloatField()
+    longitude = models.FloatField()
+
+    imagem = models.ImageField(upload_to='uploads/')
+
+    aprovado = models.BooleanField(default=False)
+    criado_por = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    criado_em = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        abstract = True
+
+
+class PontoTuristico(BaseModel):
+    pass
+
+
+class Hotel(BaseModel):
+    pass
+
+
+class Restaurante(BaseModel):
+    pass
+
+
+class Evento(BaseModel):
+    data_evento = models.DateField()
