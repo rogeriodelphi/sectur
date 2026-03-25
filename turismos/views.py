fafc13f6b0a34ca16f9837b0ebf035f922b2d98d
Rogério B. Martins <rogeriodelphi@gmail.com>
2026-03-25 02:55:06 -0300

    Criando as views(Lógica de Negócio)

--- turismos/views.py
+++ turismos/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .forms import *
+from .models import *
 
-# Create your views here.
+
+@login_required
+def cadastrar_restaurante(request):
+    if request.method == 'POST':
+        form = RestauranteForm(request.POST, request.FILES)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.criado_por = request.user
+            obj.save()
+            return redirect('home')
+    else:
+        form = RestauranteForm()
+
+    return render(request, 'form.html', {'form': form})
